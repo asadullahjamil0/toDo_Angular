251409e8864749f83dfca326b61d6a0dfaf1fd9b
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-11-15 11:48:54 +0500

    To do list small exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toDo_Angular';
+  list: any[] = [];
+  addTask(item: any) {
+    this.list.push({ id: this.list.length, name: item });
+  }
+  removeTask(id: number) {
+    this.list = this.list.filter(item => item.id !== id)
+  }
 }
